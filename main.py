50ab2c2c70c3989eef24948b1d29827c320b865d
smv307 <103333297+smv307@users.noreply.github.com>
2022-04-16 11:58:59 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import turtle
 import random
 import time
+import tkinter as tk
 from english_words import english_words_alpha_set as words
 
-yellow = "#cfbc57"
+wordle_words_set = set(line.strip() for line in open('wordList.txt'))
+
+yellow = "#b69f42"
 gray = "#525454"
 green = "#5a855a"
 
@@ -13,6 +16,39 @@ screen.addshape("Wordle.gif")
 screen.bgpic("Wordle.gif")
 screen.title("Wordle")
 
+root = tk.Tk()
+root.title("Wordle")
+
+canvas1 = tk.Canvas(root, width=400, height=300)
+canvas1.pack()
+
+entry1 = tk.Entry(root)
+canvas1.create_window(200, 140, window=entry1)
+
+label1 = None
+topLabel = tk.Label(root, text="ENTER YOUR TEXT:", fg="black")
+topLabel.config(font=("Helvetica Neue", 15, "bold"))
+canvas1.create_window(200, 70, window=topLabel)
+
+def getWord():
+    global label1
+    if label1 != None:
+        label1.destroy()
+    x = entry1.get()
+    msg = newAlg(x)
+    if msg != None:
+        label1 = tk.Label(root, text=msg, font=("Helvetica Neue", 10))
+        if msg == "You win!" or "lose" in msg:
+            time.sleep(2)
+            turtle.bye()
+            root.destroy()
+    else:
+        label1 = tk.Label(root, text="", font=("Helvetica Neue", 10))
+    canvas1.create_window(200, 240, window=label1)
+
+button1 = tk.Button(root, text="ENTER", command=getWord)
+canvas1.create_window(200, 190, window=button1)
+
 class letter_box:
     def __init__ (self, letter, hint, x, y):
         self.letter = letter.upper()
@@ -21,6 +57,9 @@ class letter_box:
         self.y = y
     def generate_square(self):
         self.letter1 = turtle.Turtle()
+        self.letter1.hideturtle()
+        self.letter1.penup()
+        self.letter1.goto(self.x, self.y)
         self.letter1.shape("square")
         if self.hint<0:
             self.letter1.color(gray)
@@ -29,23 +68,19 @@ class letter_box:
         else:
             self.letter1.color(green)
         self.letter1.shapesize(stretch_wid=4.5, stretch_len=4.5)
-        self.letter1.penup()
-        self.letter1.hideturtle()
-        self.letter1.goto(self.x, self.y)
         self.letter1.showturtle()
+        self.letter1.stamp()
         screen.update()
     def make_letter(self):
-        self.letter2 = turtle.Turtle()
-        self.letter2.hideturtle()
-        self.letter2.penup()
-        self.letter2.goto(self.x, self.y-22)
-        self.letter2.color("white")
-        self.letter2.write(self.letter, font= ("Helvetica Neue",30,"bold"), align="center")
+        self.letter1.hideturtle()
+        self.letter1.goto(self.x, self.y-22)
+        self.letter1.color("white")
+        self.letter1.write(self.letter, font= ("Helvetica Neue",30,"bold"), align="center")
         screen.update()
 
 def letters_animation(lives, hints, guessword):
     letter_list = []
-    y=260+lives*103
+    y=260-lives*103
     for i in range(5):
         letter1 = letter_box(guessword[i], hints[i], -207+i*103, y)
         letter1.generate_square()
@@ -54,14 +89,35 @@ def letters_animation(lives, hints, guessword):
 file = open("wordList.txt","r")
 wordList = file.readlines()
 word = wordList[random.randint(0, len(wordList)-1)]
+numberlives = 0
+hints = [-1, -1, -1, -1, -1]
+
+def newAlg(guess):
+    global numberlives
+    global hints
+    hints = [-1, -1, -1, -1, -1]
+    if len(guess) > 5:
+        return "Make sure you guess a five letter word."
+    for index in range(5):
+        if guess[index] in word:
+            if guess[index] == word[index]:
+                hints[index] = 1
+            else:
+                hints[index] = 0
+    letters_animation(numberlives, hints, guess)
+    numberlives += 1
+    if sum(hints) == 5:
+         return "You win!"
+    if numberlives >= 6:
+        return "You lose! \n The word was: "+word
 
 def algorithm():
     screen.update()
-    numberlives = 6
+    numberlives = 0
     guess = [""] * 5
     hints = [-1, -1, -1, -1, -1]
     print(word)
-    while numberlives > 0 and guess != word:
+    while numberlives <= 6 and guess != word:
         screen.update()
         guess = list(input("Guess a five letter word."))
         while len(guess) != 5:
@@ -72,12 +128,12 @@ def algorithm():
                     hints[index] = 1
                 else:
                     hints[index] = 0
-        numberlives-=1
         letters_animation(numberlives, hints, guess)
+        numberlives += 1
         if sum(hints) == 5:
             print("You won!")
             break
         time.sleep(0.01)
 screen.update()
-algorithm()
-#turtle.mainloop()
+root.mainloop()
+turtle.mainloop()
