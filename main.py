d109990e4470142236c71e470767e0f1fdb06302
smv307 <103333297+smv307@users.noreply.github.com>
2022-05-28 12:38:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,45 @@ topLabel = tk.Label(root, text="ENTER YOUR TEXT:", fg="white", bg=grey)
 topLabel.config(font=("Helvetica Neue", 15, "bold"))
 canvas1.create_window(200, 70, window=topLabel)
 
+root2 = tk.Tk()
+root2.title("Wordle")
+
+canvas2 = tk.Canvas(root2, width=610, height=200)
+canvas2.pack()
+
+entry2 = tk.Entry(root2)
+canvas2.config(bg=grey)
+
+letters = {"a": black, "b": black, "c": black, d: black, "e": black, "f": black, "g": black,
+           "h": black, "i": black, "j": black, "k": black, "l": black, "m": black, "n": black,
+           "o": black, "p": black, "q": black, "r": black, "s": black, "t": black, "u": black,
+           "v": black, "w": black, "x": black, "y": black, "z": black}
+
+
+def updatekeyboard():
+    x = -1
+    for key, value in letters.items():
+        x = x + 1
+        if x < 10:
+            canvas2.create_rectangle(12 + x * 60, 12, 62 + x * 60, 62, outline=grey, fill=value,
+                                     width=2)  # start x, start y, end x, end y
+            label1 = tk.Label(root2, text=key.upper(), fg="white", bg=value, font=("Helvetica Neue", 20))
+            canvas2.create_window(36 + x * 60, 38, window=label1)
+        elif x < 19:
+            canvas2.create_rectangle(40 + (x - 10) * 60, 72, 90 + (x - 10) * 60, 122, outline=grey, fill=value,
+                                     width=2)
+            label1 = tk.Label(root2, text=key.upper(), fg="white", bg=value, font=("Helvetica Neue", 20))
+            canvas2.create_window(64 + (x - 10) * 60, 98, window=label1)
+        elif x < 26:
+            canvas2.create_rectangle(88 + (x - 19) * 60, 132, 138 + (x - 19) * 60, 182, outline=grey, fill=value,
+                                     width=2)
+            label1 = tk.Label(root2, text=key.upper(), fg="white", bg=value, font=("Helvetica Neue", 20))
+            canvas2.create_window(112 + (x - 19) * 60, 158, window=label1)
+
+
+updatekeyboard()
+
+
 def getWord():
     global label1
     if label1 != None:
@@ -53,54 +92,62 @@ def getWord():
             turtle.bye()
             root.destroy()
     else:
-        label1 = tk.Label(root, text="",  fg="white", bg=grey, font=("Helvetica Neue", 10))
+        label1 = tk.Label(root, text="", fg="white", bg=grey, font=("Helvetica Neue", 10))
         canvas1.create_window(200, 240, window=label1)
+
 
 button1 = tk.Button(root, text="ENTER", command=getWord, font=("Helvetica Neue", 10, "bold"), bg="white")
 canvas1.create_window(200, 190, window=button1)
 
+
 class letter_box:
-    def __init__ (self, letter, hint, x, y):
+    def __init__(self, letter, hint, x, y):
         self.letter = letter.upper()
         self.hint = hint
         self.x = x
         self.y = y
+
     def generate_square(self):
         self.letter1 = turtle.Turtle()
         self.letter1.hideturtle()
         self.letter1.penup()
         self.letter1.goto(self.x, self.y)
         self.letter1.shape("square")
-        if self.hint<0:
+        if self.hint < 0:
             self.letter1.color(gray)
-        elif self.hint<1:
+        elif self.hint < 1:
             self.letter1.color(yellow)
         else:
             self.letter1.color(green)
+            a
         self.letter1.shapesize(stretch_wid=4.5, stretch_len=4.5)
         self.letter1.showturtle()
         self.letter1.stamp()
         screen.update()
+
     def make_letter(self):
         self.letter1.hideturtle()
-        self.letter1.goto(self.x, self.y-22)
+        self.letter1.goto(self.x, self.y - 22)
         self.letter1.color("white")
-        self.letter1.write(self.letter, font= ("Helvetica Neue",30,"bold"), align="center")
+        self.letter1.write(self.letter, font=("Helvetica Neue", 30, "bold"), align="center")
         screen.update()
+
 
 def letters_animation(lives, hints, guessword):
     letter_list = []
-    y=260-lives*103
+    y = 260 - lives * 103
     for i in range(5):
-        letter1 = letter_box(guessword[i], hints[i], -207+i*103, y)
+        letter1 = letter_box(guessword[i], hints[i], -207 + i * 103, y)
         letter1.generate_square()
         letter1.make_letter()
 
-file = open("wordList.txt","r")
+
+file = open("wordList.txt", "r")
 wordList = file.readlines()
-word = wordList[random.randint(0, len(wordList)-1)]
+word = wordList[random.randint(0, len(wordList) - 1)]
 numberlives = 0
 hints = [-1, -1, -1, -1, -1]
+
 
 def newAlg(guess):
     global numberlives
@@ -114,14 +161,23 @@ def newAlg(guess):
         if guess[index] in word:
             if guess[index] == word[index]:
                 hints[index] = 1
+                print(guess[index])
+                letters[guess[index]] = green
             else:
                 hints[index] = 0
+                if (not letters[guess[index]] == green):
+                    letters[guess[index]] = yellow
+            updatekeyboard()
+        else:
+            letters[guess[index]] = grey
+            updatekeyboard()
     letters_animation(numberlives, hints, guess)
     numberlives += 1
     if sum(hints) == 5:
-         return "You win!"
+        return "You win!"
     if numberlives >= 6:
-        return "You lose! \n The word was: "+word
+        return "You lose! \n The word was: " + word
+
 
 def algorithm():
     screen.update()
@@ -138,14 +194,20 @@ def algorithm():
             if guess[index] in word:
                 if guess[index] == word[index]:
                     hints[index] = 1
+                    print(guess[index])
+                    letters[guess[index]] = green
                 else:
                     hints[index] = 0
+                    letters[guess[index]] = yellow
+                updatekeyboard()
         letters_animation(numberlives, hints, guess)
         numberlives += 1
         if sum(hints) == 5:
             print("You won!")
             break
         time.sleep(0.01)
+
+
 screen.update()
 root.mainloop()
 turtle.mainloop()
